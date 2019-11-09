53c4756fd460d32ffd1e7b5f8ab51c2e45d50d8b
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-11-09 15:43:44 -0600

    Created Notification feature

--- Code/CourtesyShuttle/src/app/app.component.ts
+++ Code/CourtesyShuttle/src/app/app.component.ts
@@ -4,12 +4,19 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import {TabsPage} from './tabs/tabs.page';
+import {FcmService} from './fcm.service';
+import {ToastController} from '@ionic/angular';
+import { tap } from 'rxjs/operators';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+    rootPage:any = TabsPage;
   public appPages = [
       {
       title: 'Ride',
@@ -17,18 +24,33 @@ export class AppComponent {
       icon: 'list'
     }
   ];
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    constructor(platform: Platform, statusBar: StatusBar,
+                splashScreen: SplashScreen, fcm: FcmService,
+                toastCtrl: ToastController,
+                public firebaseNative: Firebase) {
+        platform.ready().then(() => {
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+            statusBar.backgroundColorByHexString('#2693ee');
+            statusBar.styleDefault();
+            splashScreen.hide();
+
+            // Get an FCM token
+            fcm.getToken();
+
+            // Listen to incoming messages
+            fcm.listenToNotifications().pipe(
+                tap(msg => {
+                    // show a toast
+                    console.log(msg);
+                    const toast = toastCtrl.create({
+                        message: msg.body,
+                        duration: 3000
+                    });
+                    //toast.present();
+                })
+            )
+                .subscribe();
+        });
+    }
+
 }
--- Code/CourtesyShuttle/src/app/app.module.ts
+++ Code/CourtesyShuttle/src/app/app.module.ts
@@ -8,15 +8,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Firebase} from '@ionic-native/firebase/ngx';
+import {FcmService} from './fcm.service';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCrZ1gtYxKK5pnMn5GQFhfPBueOrHc5xNU',
+    authDomain: 'domain.firebaseapp.com',
+    databaseURL: 'https://myfirebase-259cb.firebaseio.com',
+    projectId: 'myfirebase-259cb',
+    storageBucket: 'myfirebase-259cb.appspot.com',
+    messagingSenderId: '40687532189-aikioh2bvljd283uhb7sr9j55m700ns8.apps.googleusercontent.com'
+  }
+};
 
+// @ts-ignore
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule, AppRoutingModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFirestoreModule,
+  ],
   providers: [
+      Firebase,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    FcmService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }
   ],
   bootstrap: [AppComponent]
 })
